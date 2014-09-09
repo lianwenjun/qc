d19ca80be3ef7c303c9ef830ae8add84623f977e
lanltbl <lanchangqing@ltbl.cn>
2014-09-09 02:17:03 +0000

    修改迁移文件符合规范

--- app/database/migrations/2014_09_05_075535_create_keywords_table.php
+++ app/database/migrations/2014_09_05_075535_create_keywords_table.php
@@ -5,33 +5,33 @@ use Illuminate\Database\Schema\Blueprint;
 
 class CreateKeywordsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('keywords', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('word', 255)->comment('关键词');
-			$table->enum('is_slide', ['yes', 'no'])->default('no')->comment('是否轮播');
-			$table->integer('search_total')->default(0)->comment('自然搜索量');
-			$table->softDeletes();
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keywords', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('word', 255)->comment('关键词');
+            $table->integer('search_total')->default(0)->comment('自然搜索量');
+            $table->enum('is_slide', ['yes', 'no'])->default('no')->comment('是否轮播');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('keywords');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('keywords');
+    }
 
 }
