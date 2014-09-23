a39e99f8aefd385a03fd8dc487446fd0bbe41b72
weelion <weelion@qq.com>
2014-09-23 07:15:23 +0000

    权限配置生成 & APK上传入库功能

---
+++ app/config/upload.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'apks'     => '/apks',      // APK上传目录
+    'icons'    => '/icons',     // APK图标上传目录
+    'pictures' => '/pictures',  // APK截图上传目录
+];
--- app/views/admin/layout.blade.php
+++ app/views/admin/layout.blade.php
@@ -4,16 +4,16 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link href="{{ asset('css/admin/main.css') }}" rel="stylesheet" type="text/css" />
     <script type="text/javascript"  src="{{ asset('js/jquery.min.js') }}"></script>
-    <!--弹窗菜单-->
+    <!-- 弹窗 -->
     <link href="{{ asset('css/admin/jBox/Skins/Default/jbox.css') }}" rel="stylesheet" type="text/css" />
-    <script src="{{ asset('js/jBox/jquery.jBox-2.3.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('js/jBox/i18n/jquery.jBox-zh-CN.js') }}" type="text/javascript"></script>
-    <!--弹窗菜单-->
+    <script src="{{ asset('js/admin/jBox/jquery.jBox-2.3.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/admin/jBox/i18n/jquery.jBox-zh-CN.js') }}" type="text/javascript"></script>
+    <!--/ 弹窗 -->
 </head>
 <body>
     @yield('content')
     <div class="Content_foot">
-        <div class="Content_bottom"><a href="#">使用前必读</a> 粤ICP证030173号</div>
+        <div class="Content_bottom"><a href="javascript:alert('还没有东西哦!');">使用前必读</a> 粤ICP证030173号</div>
     </div>
 </body>
 </html>
