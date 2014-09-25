74b9fc4c127a4dc335507caa4dc4c2c80a67617a
weelion <weelion@qq.com>
2014-09-25 03:28:37 +0000

    更新游戏编辑页面

--- app/models/Cates.php
+++ app/models/Cates.php
@@ -22,4 +22,37 @@ class Cates extends \Eloquent {
                 'word' => 'required',
                 'sort' => '',
                 ];
+
+
+    /**
+     * 获取单个游戏的分类
+     *
+     * @param $id int 游戏 ID
+     *
+     * @return array [[id=>1, titile=>xxx]]
+     */
+    public function xcates($id)
+    {
+
+        $ids = AppCates::select('cate_id')
+                       ->where('app_id', $id)
+                       ->get();
+
+        print_r($ids);
+
+
+    }
+
+    /**
+     * 获取所有分类
+     *
+     * @return obj
+     */
+    public function allCates()
+    {
+        return Cates::select(['id', 'title'])
+                    ->where('parent_id', 0)
+                    ->orderBy('sort', 'desc')
+                    ->get();
+    }
 }
