dc87b1bc679871147004a212cfc92c37d3ff44ae
weelion <weelion@qq.com>
2014-09-28 07:45:13 +0000

    修改 apps 表，添加大小搜索字段

--- app/database/migrations/2014_09_05_102442_create_apps_table.php
+++ app/database/migrations/2014_09_05_102442_create_apps_table.php
@@ -19,7 +19,7 @@ class CreateAppsTable extends Migration {
             $table->string('title', 128)->comment('游戏名');
             $table->string('pack', 128)->comment('包名');
             $table->string('size', 20)->comment('游戏大小');
-            $table->integer('size_int', 10)->default(0)->unsigned()->comment('游戏大小查询用');
+            $table->integer('size_int')->unsigned()->comment('游戏大小查询用');
             $table->string('version', 32)->comment('游戏版本');
             $table->string('version_code', 32)->comment('版本代号');
             $table->string('author', 128)->comment('游戏作者');
