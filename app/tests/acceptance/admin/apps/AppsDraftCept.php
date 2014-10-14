7d6303929ec5568f6ecf5ad5cc23840cb343c99a
weelion <weelion@qq.com>
2014-10-14 02:40:12 +0000

    修改登陆状态test

--- app/tests/acceptance/admin/apps/AppsDraftCept.php
+++ app/tests/acceptance/admin/apps/AppsDraftCept.php
@@ -1,5 +1,10 @@
 <?php 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('添加编辑游戏(草稿)');
+$I->amOnPage('/admin/users/signin');
+$I->fillField('username', 'test');
+$I->fillField('password', 'test');
+$I->click(['class' => 'login_submit']);
+
 $I->amOnPage('/admin/apps/draft');
 $I->see('待编辑游戏');
