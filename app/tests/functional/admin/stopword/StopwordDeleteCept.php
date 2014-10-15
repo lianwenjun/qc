387b0122091b412dd5b5eba853196687150ec4e7
lanltbl <lanchangqing@ltbl.cn>
2014-10-15 10:16:23 +0800

    修改测试的删除功能操作

--- app/tests/functional/admin/stopword/StopwordDeleteCept.php
+++ app/tests/functional/admin/stopword/StopwordDeleteCept.php
@@ -2,10 +2,10 @@
 $I = new FunctionalTester($scenario);
 $I->wantTo('删除屏蔽词管理');
 //存在测试
-$I->amOnPage('/admin/stopword/1/delete');
+$I->sendAjaxRequest('DELETE', '/admin/stopword/1/delete');
 $I->seeSessionHasValues(['msg' => '#1删除成功']);
 
 //不存在测试
-$I->amOnPage('/admin/stopword/12/delete');
+$I->sendAjaxRequest('DELETE', '/admin/stopword/12/delete');
 $I->seeSessionHasValues(['msg' => '#12不存在']);
 
