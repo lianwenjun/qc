8fb5b1992849688e48595de442b8276ed20693f7
lanltbl <lanchangqing@ltbl.cn>
2014-10-23 15:12:09 +0800

    数据库迁移文件修改new为publish，修改上架 下架为restock，unstock

--- app/database/migrations/2014_09_05_100053_create_ads_table.php
+++ app/database/migrations/2014_09_05_100053_create_ads_table.php
@@ -22,10 +22,10 @@ class CreateAdsTable extends Migration {
             $table->string('word', 255)->comment('广告词');
             $table->integer('sort')->default(0)->unsigned()->comment('排序');
             $table->enum('type', ['index', 'app', 'editor', 'rank'])->comment('广告位分类');
-            $table->enum('is_onshelf', ['yes', 'no'])->default('no')->comment('是否上架');
+            $table->enum('is_restock', ['yes', 'no'])->default('no')->comment('是否上架');
             $table->enum('is_top', ['yes', 'no'])->default('no')->comment('是否置顶');
-            $table->timestamp('onshelfed_at')->comment('上架时间');
-            $table->timestamp('offshelfed_at')->comment('下架时间');
+            $table->timestamp('restocked_at')->comment('上架时间');
+            $table->timestamp('unstocked_at')->comment('下架时间');
             $table->softDeletes();
             $table->timestamps();
         });
