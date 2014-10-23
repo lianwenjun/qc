812cc4ab2c48f11a27a3e564b0489fd037bad4a9
weelion <weelion@qq.com>
2014-10-23 09:32:52 +0000

    修改游戏迁移文件 加索引

--- app/database/migrations/2014_09_05_102442_create_apps_table.php
+++ app/database/migrations/2014_09_05_102442_create_apps_table.php
@@ -23,10 +23,10 @@ class CreateAppsTable extends Migration {
             $table->string('md5', 32)->comment('APK md5值');
             $table->string('version', 32)->comment('游戏版本');
             $table->string('version_code', 32)->comment('版本代号');
-            $table->string('author', 128)->comment('游戏作者');
-            $table->text('summary')->comment('游戏简介');
+            $table->string('author', 128)->nullable()->comment('游戏作者');
+            $table->text('summary')->nullable()->comment('游戏简介');
             $table->text('images')->comment('游戏图片');
-            $table->text('changes')->comment('游戏特性');
+            $table->text('changes')->nullable()->comment('游戏特性');
             $table->string('reason', 20)->comment('审核不过原因');           
             $table->integer('download_counts')->default(0)->unsigned()->comment('总下载量');
             $table->string('download_manual', 32)->comment('人工用于显示下载量');
@@ -35,15 +35,21 @@ class CreateAppsTable extends Migration {
             $table->string('os', 32)->comment('系统OS');
             $table->string('os_version', 32)->comment('系统版本');
             $table->integer('sort')->default(0)->comment('排序');
-            $table->enum('status', ['publish', 'draft', 'pending', 'nopass', 'stock', 'unstock'])->default('publish')->comment('数据状态');
+            $table->enum('status', ['publish', 'draft', 'pending', 'notpass', 'stock', 'unstock'])->default('publish')->comment('数据状态');
             $table->enum('is_verify', ['yes', 'no'])->default('no')->comment('是否安全认证');
             $table->enum('has_ad', ['yes', 'no'])->default('no')->comment('是否无广告');
             $table->string('source', 10)->comment('来源');
             $table->timestamp('stocked_at')->comment('上架时间');
             $table->timestamp('unstocked_at')->comment('下架时间');
-            $table->timestamp('reviewed_at')->comment('审核时间');
             $table->softDeletes();
             $table->timestamps();
+
+            $table->index('title');
+            $table->index('pack');
+            $table->index('size_int');
+            $table->index('stocked_at');
+
+            $table->engine = 'InnoDB';
         });
     }
 
