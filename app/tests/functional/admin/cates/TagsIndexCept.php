2aa503bff6d687bb53e5434465ee6d9559ea5fb1
lanltbl <lanchangqing@ltbl.cn>
2014-10-23 17:00:23 +0800

    分类的修改结果

--- app/tests/functional/admin/cates/TagsIndexCept.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$I = new FunctionalTester($scenario);
-$I->wantTo('游戏标签列表页');
-$I->amOnPage('admin/tag/index');
-//检测ACTION和ROUTENAME
-$I->seeCurrentRouteIs('tag.index');
-$I->seeCurrentActionIs('Admin_CatesController@tagIndex');
-$I->see('游戏标签管理');
