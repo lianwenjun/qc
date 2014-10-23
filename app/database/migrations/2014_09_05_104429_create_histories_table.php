09e932a0661244dd6bed78020ceecd3658944283
lanltbl <lanchangqing@ltbl.cn>
2014-10-23 15:12:09 +0800

    数据库迁移文件修改new为publish，修改上架 下架为restock，unstock

--- app/database/migrations/2014_09_05_104429_create_histories_table.php
+++ app/database/migrations/2014_09_05_104429_create_histories_table.php
@@ -21,6 +21,7 @@ class CreateHistoriesTable extends Migration {
             $table->string('pack', 128)->comment('包名');
             $table->string('size', 20)->comment('游戏大小');
             $table->integer('size_int')->unsigned()->comment('游戏大小查询用');
+            $table->string('md5', 32)->comment('APK md5值');
             $table->string('version', 32)->comment('游戏版本');
             $table->string('version_code', 32)->comment('版本代号');
             $table->string('author', 128)->comment('游戏作者');
@@ -35,11 +36,12 @@ class CreateHistoriesTable extends Migration {
             $table->string('os', 32)->comment('系统OS');
             $table->string('os_version', 32)->comment('系统版本');
             $table->integer('sort')->default(0)->comment('排序');
-            $table->enum('status', ['new', 'draft', 'pending', 'nopass', 'onshelf', 'offshelf'])->default('new')->comment('数据状态');
+            $table->enum('status', ['publish', 'draft', 'pending', 'nopass', 'restock', 'unstock'])->default('publish')->comment('数据状态');
             $table->enum('is_verify', ['yes', 'no'])->default('no')->comment('是否安全认证');
             $table->enum('has_ad', ['yes', 'no'])->default('no')->comment('是否无广告');
-            $table->timestamp('onshelfed_at')->comment('上架时间');
-            $table->timestamp('offshelfed_at')->comment('下架时间');
+            $table->string('source', 10)->comment('来源');
+            $table->timestamp('restocked_at')->comment('上架时间');
+            $table->timestamp('unstocked_at')->comment('下架时间');
             $table->timestamp('reviewed_at')->comment('审核时间');
             $table->text('cates')->comment('游戏分类');
             $table->text('tags')->comment('游戏标签');
