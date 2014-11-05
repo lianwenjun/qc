63b8b925afc28a548ab7a53c73cf7b633fd9744c
zhaohuanltbl <zhaohuan@ltbl.cn>
2014-11-05 06:14:02 +0000

    应用反馈测试重写

--- app/tests/functional/admin/feedback/FeedbackIndexCept.php
+++ app/tests/functional/admin/feedback/FeedbackIndexCept.php
@@ -2,30 +2,31 @@
 $I = new FunctionalTester($scenario);
 $I->wantTo('应用反馈列表功能');
 
-//登陆
+// 登陆
 $I->sendAjaxRequest('put', '/admin/users/signin', ['username' => 'test', 'password' => 'test']);
 
 $I->amOnRoute('feedback.index');
-$I->see('应用反馈');
+$I->see('应用反馈列表');
 
-//搜索
+// ID搜索
 $I->selectOption('cate', 'id');
 $I->fillField('keyword', '1');
 $I->click('.Search_en');
-$I->dontSee('没数据', '//table');
-$I->seeNumberOfElements('//table/tr', 2);
+$I->see('应用反馈列表第一行内容', '//table');
 
-
-//搜索错误
-$I->selectOption('cate', 'content');
-$I->fillField('keyword', 'hehe');
-$I->click('.Search_en');
-$I->see('没数据');
-
-$I->sendAjaxGetRequest('/admin/feedback/index', ['date' => ['2014-11-16', '2014-11-15']]);
-$I->see('没数据');
-
+// IMEI搜索
 $I->selectOption('cate', 'imei');
-$I->fillField('keyword', 'hehe');
-$I->click('搜索');
-$I->see('没数据');
+$I->fillField('keyword', 'ABCDEF');
+$I->click('.Search_en');
+$I->see('应用反馈列表第二行内容', '//table');
+
+// 内容搜索
+$I->selectOption('cate', 'content');
+$I->fillField('keyword', '第二行');
+$I->click('.Search_en');
+$I->see('应用反馈列表第二行内容', '//table');
+
+// 时间搜索
+$I->amOnPage('/admin/feedback/index?cate=&keyword=&date[]=2014-10-17&date[]=2014-10-18');
+$I->see('应用反馈列表第三行内容', '//table');
+
