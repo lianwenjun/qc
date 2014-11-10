a32191fb2f41e7b8565c6543105131e90110f573
zhaohuanltbl <zhaohuan@ltbl.cn>
2014-11-10 02:22:39 +0000

    应用反馈测试修改

--- app/tests/functional/admin/feedback/FeedbackIndexCept.php
+++ app/tests/functional/admin/feedback/FeedbackIndexCept.php
@@ -9,24 +9,24 @@ $I->amOnRoute('feedback.index');
 $I->see('应用反馈列表');
 
 // ID搜索
-$I->selectOption('cate', 'id');
+$I->selectOption('type', 'id');
 $I->fillField('keyword', '1');
 $I->click('.Search_en');
 $I->see('应用反馈列表第一行内容', '//table');
 
 // IMEI搜索
-$I->selectOption('cate', 'imei');
+$I->selectOption('type', 'imei');
 $I->fillField('keyword', 'ABCDEF');
 $I->click('.Search_en');
 $I->see('应用反馈列表第二行内容', '//table');
 
 // 内容搜索
-$I->selectOption('cate', 'content');
+$I->selectOption('type', 'content');
 $I->fillField('keyword', '第二行');
 $I->click('.Search_en');
 $I->see('应用反馈列表第二行内容', '//table');
 
 // 时间搜索
-$I->amOnPage('/admin/feedback/index?cate=&keyword=&date[]=2014-10-17&date[]=2014-10-18');
+$I->amOnPage('/admin/feedback/index?type=&keyword=&created_at[]=2014-10-17&created_at[]=2014-10-18');
 $I->see('应用反馈列表第三行内容', '//table');
 
