6397a3aa2d568d18c89eb426e38e6610ab55c53d
RavenLee <liweibin@ltbl.cn>
2014-11-18 07:23:22 +0000

    app_downloads表添加字段

--- app/database/migrations/2014_11_18_151745_add_download_percent_to_app_downloads.php
+++ app/database/migrations/2014_11_18_151745_add_download_percent_to_app_downloads.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAppDownloadsTable extends Migration {
+class AddDownloadPercentToAppDownloads extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,10 @@ class CreateAppDownloadsTable extends Migration {
 	 */
 	public function up()
 	{
-        //
+		Schema::table('app_downloads', function($table)
+		{
+			$table->decimal('download_percent', 5, 2)->default(0)->comment('下载占比(安装量/下载量)');
+		});
 	}
 
 	/**
