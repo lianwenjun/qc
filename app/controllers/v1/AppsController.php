7ccd72ca7bc6f820216c69086b19d73a64474115
lanltbl <lanchangqing@ltbl.cn>
2014-12-11 15:43:29 +0800

    修改分类新的空数据的时候的BUG

--- app/controllers/v1/AppsController.php
+++ app/controllers/v1/AppsController.php
@@ -96,6 +96,9 @@ class V1_AppsController extends \V1_BaseController {
             $exIds = $exIds ? unserialize($exIds) : [];
         }
         $appIds = array_except($appIds, [$exclude] + $exIds);
+        if (!$appIds) {
+            return ['count' => 0, 'apps' => []];
+        }
         $count = Api_Apps::whereStatus('stock')
             ->whereIn('id', $appIds)
             ->count();
