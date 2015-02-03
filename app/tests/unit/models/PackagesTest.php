6e85421f48e243c90ffc7e337f4999ed025a45cc
chenruimin <chenruimin@ltbl.cn>
2015-02-03 17:10:14 +0800

    Packages model的测试

---
+++ app/tests/unit/models/PackagesTest.php
@@ -0,0 +1,30 @@
+<?php
+
+class PackagesTest extends TestCase
+{
+
+    // 测试豌豆荚数据
+    public function testWdjRefresh()
+    {
+        $this->assertTrue(Packages::refresh('com.halfbrick.fruitninjafree', 'wdj'));
+    }
+
+    // 测试应用宝数据
+    public function testYybRefresh()
+    {
+        $this->assertTrue(Packages::refresh('com.halfbrick.fruitninjafree', 'yyb'));
+    }
+
+    // 测试应用宝数据(这个包是不存在的)
+    public function testYybRefresh()
+    {
+        $this->assertFalse(Packages::refresh('com.halfbrick.fruitninjafree111', 'yyb'));
+    }
+
+    // 测试整个列表
+    public function testRefreshFromApps()
+    {
+        $this->assertTrue(Packages::refreshListFromApps());
+    }
+
+}
